6723e905230febe26e51fe517fce3b9725abaec4
gabriel <ggabrieldeveloper@gmail.com>
2023-07-03 23:31:24 -0300

    modificate

--- sandbox/17-2-lists-methods.py
+++ sandbox/17-2-lists-methods.py
@@ -13,3 +13,24 @@ print(f'3 - {lista_mascotas}')
 otros_animales = ['Cuervo', 'Iguana', 'Cerdo']
 lista_mascotas.extend(otros_animales)
 print(f'4 - {lista_mascotas}')
+
+lista1 = [1, 12, 123]
+lista2 = ["Bye", "Ciao", "Agur", "Adieu"]
+
+# completa el ejercicio
+lista1.append(1234)
+lista1.append('Hola')
+
+lista2.append('Adiós')
+lista2.append(1234)
+print(lista1)
+print(lista2)
+lista3 = []
+lista3.extend(lista1[0:len(lista1) - 1])
+print(lista3)
+
+lista4 = []
+lista4.extend(lista2[1:len(lista2) - 1])
+print(lista4)
+
+lista5 = lista4 + lista3
